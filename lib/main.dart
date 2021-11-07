bb8fef8f87fe7f06f7617c6076bd8d8489070e76
Al Amin <alamin.mistrisolutions@gmail.com>
2021-11-07 12:51:20 +0600

    simple routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,17 @@
+
 import 'package:demo/pages/home.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const MaterialApp(home: Home()));
+import '/pages/choose_location.dart';
+import '/pages/loading.dart';
+
+void main() => runApp(
+  MaterialApp(
+    initialRoute: '/home',
+    routes: {
+      '/' : (context) => Loading(),
+      '/home' : (context) => Home(),
+      '/location' : (context) => ChooseLocation()
+    },
+  )
+);
